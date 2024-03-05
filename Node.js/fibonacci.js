66d0551b088ba399f97657ed6a7ba4aaf79fbdf9
Javier Crisóstomo <crisostomo.almeyda@gmail.com>
2024-03-05 13:58:10 -0500

    Minor changes.

--- Node.js/fibonacci.js
+++ Node.js/fibonacci.js
@@ -7,6 +7,9 @@ Tenemos una función que recibe un entero y devuelve un entero
 
     0 1 2 3 4 5 6 7  8  9  10 11 // Entradas
     0 1 1 2 3 5 8 13 21 34 55 89 // Salidas
+
+    No usar recursividad porque tiene complejidad algoritmica exponencial
+    (2^n) y se puede hacer con complejidad lineal (n).
 */
 // Solution:
 //No usar recursividad
